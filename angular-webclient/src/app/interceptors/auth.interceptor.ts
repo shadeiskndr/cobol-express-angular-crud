d3dd9b0d896356f5a210020d012b3a9d115c03f6
Shahathir Iskandar <shahathiriskandar42@gmail.com>
2025-04-11 17:58:09 +0800

    refactor: convert AuthInterceptor to functional interceptor
    This commit refactors the `AuthInterceptor` from a class-based interceptor to a functional interceptor.
    
    The following changes were made:
    
    - Converted `AuthInterceptor` to a functional interceptor using `HttpInterceptorFn`.
    - Used `inject` to access `AuthService` within the functional interceptor.
    - Updated the spec file to reflect the change to a functional interceptor.

--- angular-webclient/src/app/interceptors/auth.interceptor.ts
+++ angular-webclient/src/app/interceptors/auth.interceptor.ts
@@ -1,31 +1,18 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    const token = this.authService.getToken();
-
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    }
-
-    return next.handle(request);
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
-}
+
+  return next(req);
+};
